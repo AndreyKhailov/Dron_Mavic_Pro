6fcd68fb3a99edab69777c9ad1cabc283d6da29c
Andrey Khaylov <andrey.haylovv@yandex.ru>
2021-04-05 22:26:16 +0300

    corrected html and style.css files

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,7 @@
 $(function() {
 
-    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    // $(".description__slider").slick({    //!!!!
-    //     prevArrow: " ",                  //!!!!
-    //     nextArrow: " ",                  //!!!!
-    //     infinite: false,                 //!!!!
-    // })                                   //!!!!
-    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
     $(".questions__article-title").on("click", function() {
         $(".questions__article").removeClass("questions__article--active")
         $(this).parent().addClass("questions__article--active")
     })
-
 })
