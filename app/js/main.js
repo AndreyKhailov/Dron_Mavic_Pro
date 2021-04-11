875b342be4ee01cd6e3292984940bf20f4865e6c
Andrey Khaylov <andrey.haylovv@yandex.ru>
2021-04-11 22:19:13 +0300

    created mobile menu

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,25 @@ $(function() {
         $(this).parent().addClass("questions__article--active")
     })
 })
+
+// ************** Create mobile menu ******************************
+
+const   burgerBtn = document.getElementById('menu-burger')
+        mobileMenu = document.getElementById('menu')  
+// Mobile menu is opened and closed by clicking on the button menu       
+burgerBtn.addEventListener('click', () => {
+    // Open and close menu button -->
+    burgerBtn.classList.value === "header__menu-burger btn--active" 
+        ? burgerBtn.classList.remove('btn--active')
+        : burgerBtn.classList.add('btn--active');
+    // Open and close mobile menu -->
+    mobileMenu.classList.value === "header__menu-list menu--active" 
+        ? mobileMenu.classList.remove('menu--active')
+        : mobileMenu.classList.add('menu--active');  
+});
+// Mobile menu is closed by clicking on the menu links
+mobileMenu.addEventListener('click', () => {
+    burgerBtn.classList.remove('btn--active');
+    mobileMenu.classList.remove('menu--active');
+});
+// ****************************************************************
